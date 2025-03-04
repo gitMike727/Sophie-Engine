444cb2053b76b112032e1a8beb3d8449b474c03e
mtruong727 <mtruong727@outlook.com>
2025-03-04 13:57:59 -0600

    Remove Files

--- Layer.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class Layer {
-public:
-	virtual void Render() = 0;
-	virtual void Update() = 0;
-};
--- Main.cpp
+++ Main.cpp
@@ -1,35 +1,40 @@
-#include "Engine.h"
-#include "Timer.h"
-#include "Entity.h"
-#include "ECS.h"
-#include "EntityManager.h"
-#include "Transform.h"
+#include <iostream>
+#include <SDL.h>
 
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
 
-
-int main(int argc, char* argv[])
+int main(int argc, char* args[])
 {
-	EntityManager* manager = new EntityManager();	
-	Entity* player = new Entity();
-
-	manager->addEntity(player);
-	player->GetComponent<Transform>().position = Vector2Df(0,0);
-
-	Engine::GetInstance()->Init();
-
-	while (Engine::GetInstance()->IsRunning())
+	// Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		Engine::GetInstance()->Event();
-		Engine::GetInstance()->Update();
-		Engine::GetInstance()->Render();
-		Timer::GetInstance()->Tick();
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	// Create window
+	SDL_Window* window = SDL_CreateWindow("SOPHIE ENGINE | Tiny Bait", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == NULL)
+	{
+		std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+	// Create renderer for window
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	if (renderer == NULL)
+	{
+		std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+		return 1;
 	}
 
-
-
-	Engine::GetInstance()->Clean();
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	SDL_Delay(2000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
-
-
 }
-	
+
+
+
