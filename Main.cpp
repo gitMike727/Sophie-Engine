21f4cb0de0230cf21a9db6180cd7689fa188d212
mtruong727 <mtruong727@outlook.com>
2025-03-04 16:39:44 -0600

    Update Tiny_Bait_Project

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,7 @@
-#include <stdlib.h>
-#include <time.h>
-#include "Graphics.h"
-#include "game/world.h"
-#include "input/inputs.h"
+#include "Engine.h"
+#include "SDL.h"
 
-int main() {
-
-    // Initialize random seed
-    srand(time(NULL));
-
-    Inputs inputs;
-    Graphics graphics;
-    World world(graphics.get_width(), graphics.get_height());
-
-    bool loop = true;
-
-    while (loop) {
-
-        inputs.update();
-        loop = !inputs.get_quit();
-
-        world.update(&inputs);
-
-        graphics.clear_screen();
-        graphics.render_entities(world.get_entities(), world.get_delta());
-        graphics.render_overlay(world.get_entities());
-
-        graphics.present_renderer(world.get_delta());
-
-    }
-
-    return 0;
+int main(int argc, char* argv[])
+{
+	
+}
