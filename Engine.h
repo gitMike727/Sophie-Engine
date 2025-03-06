fd5023027e623c04c890f73185d41c49efa71619
mtruong727 <mtruong727@outlook.com>
2025-03-06 14:49:49 -0600

    Update Tiny_Bait_Project

--- Engine.h
+++ Engine.h
@@ -1,32 +1,5 @@
 #pragma once
-
-#include <string>
-#include <vector>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#include "AssetManager.h"
-#include "FPSCounter.h"
-#include "Entity.h"
-
 class Engine
 {
-
-	static const int WIDTH, HEIGHT;
-
-	FPSCounter fps_counter;
-	AssetManager* assetManager;
-	SDL_Window* window;
-	SDL_Renderer* renderer;
-
-	bool Init();
-
-public:
-	Engine();
-	void ClearScreen();
-	void Render(float dt);
-	int get_width();
-	int get_height();
-	~Engine();
 };
 
